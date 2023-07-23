d7b1e9d315027d8ab857ae261fd7f480ebdcb768
Ten <32621403+TenDRILLL@users.noreply.github.com>
2023-07-23 11:35:54 +0300

    Update and rename EventHandler.cs to EventHandler.cs

--- SCP_Kick/com.github.sekasin.scp_kick/EventHandler.cs
+++ SCP_Kick/com.github.sekasin.scp_kick/EventHandler.cs
@@ -3,7 +3,7 @@ using Exiled.Events.EventArgs.Player;
 using Exiled.Permissions.Extensions;
 using MEC;
 
-namespace SCP_Kick.com.github.tendrilll.scp_kick {
+namespace SCP_Kick.com.github.sekasin.scp_kick {
     public class EventHandler {
         private readonly bool _debugMode;
         private readonly Plugin<Config> _main;
@@ -22,10 +22,10 @@ namespace SCP_Kick.com.github.tendrilll.scp_kick {
                     Log.Info("Checking for permission " + _main.Config.Permission + ": " + args.Player.CheckPermission(_main.Config.Permission));
                     Log.Info("Checking if NW staff: " + args.Player.IsNorthwoodStaff);
                 }
-                //This cryptic statement translates as: you can play if you have the perm OR you're NW staff and the bypass is on.
-                if (!args.Player.CheckPermission(_main.Config.Permission) || (args.Player.IsNorthwoodStaff && !_main.Config.NWBypass)) {
-                    args.Player.Kick(_main.Config.KickMessage);
-                }
+                if (args.Player.CheckPermission(_main.Config.Permission)) return;
+                if (args.Player.IsNorthwoodStaff && _main.Config.NWBypass) return;
+                
+                args.Player.Kick(_main.Config.KickMessage);
             });
         } 
     }
